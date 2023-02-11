f3b2c8d6172fca7adf30ab59b013e87a0f7f75a0
Sarvesh Warge <sarveshwarge@gmail.com>
2023-02-11 20:38:41 +0530

    modifies sync logic

--- index.ts
+++ index.ts
@@ -1,26 +1,26 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 // Routes
-import auth from './src/microservices/auth/auth.route';
-import user from './src/microservices/user/user.route';
-import periodTracking from './src/microservices/periodTracking/periodTracking.route';
-import admin from './src/microservices/admin/admin.route';
+import auth from "./src/microservices/auth/auth.route";
+import user from "./src/microservices/user/user.route";
+import periodTracking from "./src/microservices/periodTracking/periodTracking.route";
+import admin from "./src/microservices/admin/admin.route";
 
-import { cleaner } from './src/middleware/cleaner';
+import { cleaner } from "./src/middleware/cleaner";
 
 // Import essential functions
-import cors from 'cors'
-import morgan from 'morgan'
-import helmet from 'helmet'
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-import db from './src/models';
-import { users } from './src/seeders/users';
-import errorMiddleware from './src/middleware/error'
+import db from "./src/models";
+import { users } from "./src/seeders/users";
+import errorMiddleware from "./src/middleware/error";
 
 // Add functions to app
 app.use(cors());
@@ -35,34 +35,28 @@ app.set("etag", false);
 // remove any null or undefined keys from the request body
 app.use("/", cleaner);
 
-
 app.use("/auth", auth);
 app.use("/user", user);
 app.use("/periodTracking", periodTracking);
 app.use("/admin", admin);
 app.use(errorMiddleware);
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 // heahth check route
 app.get("/health-check", (req, res, next) => {
-    res.status(200).json({ message: "Health check successful" });
+  res.status(200).json({ message: "Health check successful" });
 });
 
 if (process.env.SYNC) {
-    db.sequelize.sync({ alter: true }).then(() => {
-        app.listen(port, () => {
-            console.log(`App listening on port with sync ${port}`)
-        })
-    })
+  db.sequelize.sync({ alter: true }).then(() => {
+    console.log(`DB sync completed`);
+    process.exit(0);
+  });
 } else {
-    app.listen(port, () => {
-        console.log(`App listening on port ${port}`)
-    })
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
 }
-
-
-
